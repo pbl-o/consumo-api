fa6bc4e0077dc6f28c9e1178cd1f4b030b3f0378
pblo <pdiazarenas541@gmail.com>
2025-09-27 00:01:14 +0100

    add apiFethcer util

--- src/utils/apiFetcher.js
+++ src/utils/apiFetcher.js
@@ -1,8 +1,8 @@
-  const getAllPizzasApi = async(url, dataSource, stateSetter) =>{
+  const apiFetcher = async(url, dataSource, stateSetter) =>{
     const res = await fetch(url)
     const data = await res.json()
     stateSetter(data)
     console.log(`${dataSource} fetch was succesful!`)
   }
 
-  export default getAllPizzasApi
+  export default apiFetcher
